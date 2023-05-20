aebc5e021542e75b5442cf265d14e4ee456c2b87
Imanullah <pengguna21@gmail.com>
2023-05-20 21:47:41 +0700

    fix | port

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  serverRuntimeConfig: {
-    port: 4000, // Replace with your desired port number
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
